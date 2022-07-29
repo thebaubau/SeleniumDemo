2cc9a2669615d653d554d11a59249a7ed86208ef
Daniel Fintinariu <fintinariu.daniel@gmail.com>
2022-07-29 12:42:25 +0200

    Simple test to open Medium and check title

---
+++ src/test/java/OpenMediumTest.java
@@ -0,0 +1,39 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class OpenMediumTest {
+
+    WebDriver driver;
+
+    @BeforeAll
+    static void setupAll() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setup() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void teardown() {
+        driver.quit();
+    }
+
+    @Test
+    void test() {
+        driver.get("https://medium.com/");
+        String title = driver.getTitle();
+
+        // Verify
+        assertThat(title).contains("Medium");
+    }
+
+}
